a8a5669997be0aeb69d1fe72c72e7428ef0a248b
Daniel Lemmon <95183188+danielwlemmon@users.noreply.github.com>
2022-04-04 21:04:18 -0600

    added styling/car info
    Photos all added, along with description text.  Styling added, but not complete.

---
+++ index.js
@@ -0,0 +1,51 @@
+$(document).ready(function () {
+  $("#tesla").hover(
+    function () {
+      $("#tesla_info").show();
+    },
+    function () {
+      $("#tesla_info").hide();
+    }
+  );
+});
+
+$(document).ready(function () {
+  $("#ford").hover(
+    function () {
+      $("#ford_info").show();
+    },
+    function () {
+      $("#ford_info").hide();
+    }
+  );
+});
+
+$(document).ready(function(){
+  $(".make").hover(function(){
+      $(this).css("background-color", "lightgray");
+  }, function(){
+      $(this).css("background-color", "gray");
+  });
+});
+
+$(document).ready(function () {
+  $("#rivian").hover(
+    function () {
+      $("#rivian_info").show();
+    },
+    function () {
+      $("#rivian_info").hide();
+    }
+  );
+});
+
+$(document).ready(function () {
+  $("#polestar").hover(
+    function () {
+      $("#polestar_info").show();
+    },
+    function () {
+      $("#polestar_info").hide();
+    }
+  );
+});
